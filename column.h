ed91db48370203767054b25cd144e7317f1c8499
RaphaelBilly <raphael.billy@efrei.net>
2024-03-25 16:10:33 +0100

    added column.c

--- column.h
+++ column.h
@@ -4,7 +4,17 @@
 
 #ifndef PROJET_LANGAGE_C_COLUMN_H
 #define PROJET_LANGAGE_C_COLUMN_H
-// test
 
 
+typedef int Data;
+
+typedef struct {
+    char* title;
+    int physicalSize;
+    int logicalSize;
+    Data* values;
+} Column;
+
+void insertValue(Column *column, Data value);
+Column *createColumn(char* title);
 #endif //PROJET_LANGAGE_C_COLUMN_H
