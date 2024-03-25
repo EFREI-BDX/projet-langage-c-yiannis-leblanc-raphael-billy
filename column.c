19b654b43f19e7e0097d6a963668ac6663ffa85b
RaphaelBilly <raphael.billy@efrei.net>
2024-03-25 16:10:56 +0100

    added column.c

---
+++ column.c
@@ -0,0 +1,9 @@
+//
+// Created by nogel on 25/03/2024.
+//
+
+Column *createColumn(char* title){
+    Column *column = (Column*) malloc(sizeof(Column));
+    //plein de machins
+    return column;
+}
