86f2718aca42a2279d430de15e4a090ff8d2c3a0
Yiannis Leblanc <yiannis.leblanc@efrei.net>
2024-04-21 19:29:39 +0200

    Rectification finale, prototypes du CDataframe.h

--- CDataframe.h
+++ CDataframe.h
@@ -21,16 +21,18 @@ void print_cdata(CDataframe tab);
 void print_cdata_lines(CDataframe tab, int line1, int line2);
 void print_cdata_lines_input(CDataframe tab);
 void print_cdata_col(CDataframe tab, int col1, int col2);
-void add_line(CDataframe tab);
-void del_line(CDataframe tab, int line);
-void add_col(CDataframe tab);
+void print_cdata_col_input(CDataframe tab);
+void add_line(CDataframe* tab);
+void del_line(CDataframe* tab, int line);
+void add_col(CDataframe* tab);
+void del_col(CDataframe* tab, int line);
 void rename_cdata_col(CDataframe tab, int col);
-void in_cdata(CDataframe tab, Data x);
+int in_cdata(CDataframe tab, Data x);
 int nb_ligne(CDataframe tab);
 int nb_colonne(CDataframe tab);
 int nb_equal_cdata(CDataframe tab, Data x);
-void nb_higher_cdata(CDataframe tab, Data x);
-void nb_lower_cdata(CDataframe tab, Data x);
-void nom_colonne(CDataframe tab);
+int nb_higher_cdata(CDataframe tab, Data x);
+int nb_lower_cdata(CDataframe tab, Data x);
+void print_col_names(CDataframe tab);
 
 #endif
