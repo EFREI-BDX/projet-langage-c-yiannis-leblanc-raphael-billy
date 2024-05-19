a0fe13a1936b1aa08cbc641ce7ace70408daf081
RaphaelBilly <raphael.billy@efrei.net>
2024-05-19 14:49:15 +0200

    added part 2

--- column.h
+++ column.h
@@ -19,6 +19,35 @@ typedef struct {
     Data* values;
 } Column;
 
+
+enum enum_type
+{
+    NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+};
+typedef enum enum_type ENUM_TYPE;
+
+union column_type{
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    char* string_value;
+    void* struct_value;
+};
+typedef union column_type COL_TYPE ;
+
+
+struct column {
+    char *title;
+    unsigned int size; //logical size
+    unsigned int max_size; //physical size
+    ENUM_TYPE column_type;
+    COL_TYPE **data; // array of pointers to stored data
+    unsigned long long int *index; // array of integers
+};
+typedef struct column COLUMN;
+
 void insertValue(Column *column, Data value);
 
 Column* createColumn(char* title);
